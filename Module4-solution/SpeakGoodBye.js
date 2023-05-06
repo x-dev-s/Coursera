6343f16b8ba4c5c5d2fc7fb380af2ceed0248c73
Abdullah Sultan <abdullahsultan546@gmail.com>
2023-05-06 17:53:25 +0500

    done

--- Module4-solution/SpeakGoodBye.js
+++ Module4-solution/SpeakGoodBye.js
@@ -19,9 +19,9 @@
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
   byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+    console.log(byeSpeaker.speakWord + " " + name);
   }
-  window.byeSpeaker = byeSpeaker
+  window.byeSpeaker = byeSpeaker;
 })(window);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -35,14 +35,13 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
 (function(window){
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   // STEP 10:
   // Loop over the names array and say either 'Hello' or "Good Bye"
   // using either the helloSpeaker's or byeSpeaker's 'speak' method.
   // See Lecture 50, part 1
   for (var name in names) {
-
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
     // Use the string object's 'charAt' function. Since we are looking for
@@ -51,7 +50,7 @@ WARNING!!! WARNING!!!
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
     // var firstLetter =
-    var firstletter = tolowerCase(name.charAt(0))
+    var firstletter = names[name].toLowerCase().charAt(0)
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -59,9 +58,9 @@ WARNING!!! WARNING!!!
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if (firstletter == 'j') {
-      byeSpeaker.speak(name)
+      byeSpeaker.speak(names[name])
     } else {
-      helloSpeaker.speak(name)
+      helloSpeaker.speak(names[name])
     }
   }
 })(window);
